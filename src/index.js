4fe2f6e0dbab380f7d3d485f12d1806a35a1157d
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-07-25 20:40:47 +0600

    mongodb connected

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
 
-import mongoose from "mongoose";
-import connectDB from "./db";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
 
 connectDB();
 
