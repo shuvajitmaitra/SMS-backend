e2a754893eef63b9ad825b6125a50fadb82492d1
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-08-02 17:19:38 +0600

    initial files and folders setup also added the error handler and api handler now the time to create models

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,9 @@
+const asyncHandler = (requestHandler) => (req, res, next) => {
+  Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
+};
+
+export { asyncHandler };
+
 // const asyncHandler = (fn) => async (req, res, next) => {
 //   try {
 //     await fn(req, res, next);
@@ -8,9 +14,3 @@
 //     });
 //   }
 // };
-
-const asyncHandler = (requestHandler) => (req, res, next) => {
-  Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
-};
-
-export { asyncHandler };
