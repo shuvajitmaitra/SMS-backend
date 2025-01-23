7e14372ca108aecfa2032d6d8d87997f4267a25e
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-01-23 23:05:06 +0600

    database change

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(
   })
 );
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
--- src/db/index.js
+++ src/db/index.js
@@ -3,12 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
-    console.log(
-      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
-    );
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.log("Error to connect database", error);
     process.exit(1);
